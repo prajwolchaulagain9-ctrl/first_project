20895d6fd41c21b2451540e532a4d758914ab3fa
Prajwol <prajwolchaulagain9@gmail.com>
2025-11-16 12:19:13 +0545

    ramrari kaam garao

---
+++ fx.py
@@ -0,0 +1,35 @@
+# name=input("Enter your name: ")
+# first=name.lower()
+# then= first.strip()
+# final= then.split()
+# print("_".join(final))
+
+# password= input("Enter your password: ")
+
+# step1=password.lower()
+# step2= step1.replace("a","@")
+# step3= step2.replace("e","3")
+# step4= step3.replace("i","1")
+# step5= step4.replace("o","0")
+# step6= step5.replace("s","$")
+# print("Your secret code is",step6+"0##9")
+
+# email= input("Enter your email: ")
+# step1=email.rfind("@")
+# step2=email[:step1]
+# print("Your username is",step2)
+
+# number= input("Enter a number: ")
+# step1= number.replace(" ","").replace("-","").replace("+","")
+
+
+# print("Your number is",step1)
+
+
+
+
+
+# name= input("Enter your name: ")
+# first= name.lower()
+# then= first.title()
+# print(then)
